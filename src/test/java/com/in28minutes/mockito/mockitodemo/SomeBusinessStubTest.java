448ef2200891099a3e9635b2a3ced8fc7d3609a0
Rajeev Singh <rajeevsinghup30@gmail.com>
2022-05-26 17:53:22 +0530

    First Stub Test

---
+++ src/test/java/com/in28minutes/mockito/mockitodemo/SomeBusinessStubTest.java
@@ -0,0 +1,23 @@
+package com.in28minutes.mockito.mockitodemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SomeBusinessStubTest {
+
+    @Test
+    public void testfindTheGreatestFromAllDat(){
+        SomeBusinessImpl businessImpl = new SomeBusinessImpl(new DataServiceStub());
+        int result =  businessImpl.findTheGreatestFromAllData();
+       assertEquals(24,result);
+    }
+}
+
+class DataServiceStub implements  DataService{
+    @Override
+    public int[] retrieveAllData(){
+        return new int[] {24,6,15};
+    }
+
+}
